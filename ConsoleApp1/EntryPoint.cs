529b3563d9df0fde09f7496264625fe0b8246b9f
alfcastillo90 <cacr1990@gmail.com>
2018-04-15 18:36:33 -0300

    primer commit

---
+++ ConsoleApp1/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using PointAndLines;
+using System;
+class EntryPoint
+{
+    static void Main()
+    {
+        User user = new User("Vachev",Race.Earthling);
+        User secondUser = new User();
+        User thirdUser = new User();
+        Console.WriteLine(user.ID);
+        Console.WriteLine(secondUser.ID);
+        Console.WriteLine(thirdUser.ID);
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,76 @@
+﻿namespace PointAndLines
+{
+    class User
+    {
+        public readonly int HEIGHT;
+        public readonly int ID;
+
+        public static int currentID;
+
+        public Race race;
+
+        private string username;
+        private int password;
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                if (value.Length >= 4 && value.Length <= 10) {
+                    username = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Oops!. This is not a valid username");
+                }
+
+            }
+        }
+
+        public int Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (value >= 4 && value <= 10)
+                {
+                    password = value;
+                }
+                else
+                {
+                    System.Console.WriteLine("Oops!. This is not a valid password");
+                }
+
+            }
+        }
+
+        public User ()
+        {
+            currentID++;
+            ID = currentID;
+        }
+        public User (string username, Race race)
+        {
+            currentID++;
+            ID = currentID;
+            this.username = username;
+            if (race == Race.Marsian)
+            {
+                HEIGHT = 100;
+            }
+            else if( race == Race.Earthling)
+            {
+                HEIGHT = 180;
+            }
+        }
+    }
+}
+
+
